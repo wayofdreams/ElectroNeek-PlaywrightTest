a46a2c6db885aa1c09f4bce0d5623340721bc383
Rafael Contreras <wayofdreams.rc@gmail.com>
2023-01-15 00:03:41 -0400

    Finished positive tests and cleaned code

--- tests/negative.spec.ts
+++ tests/negative.spec.ts
@@ -7,7 +7,6 @@ test("Email and password are requiered", async ({ page }) => {
     const passwordError = await page.locator("#mat-error-1");
     await expect(emailError).toContainText("E-mail is required");
     await expect(passwordError).toContainText("Password is required");
-    //await delay(4000);
 })
 
 test("Email and password do not match", async ({ page }) => {
@@ -17,29 +16,4 @@ test("Email and password do not match", async ({ page }) => {
     await page.click("text = Continue");
     const errorMessage = await page.locator(".fuse-alert-message");
     await expect(errorMessage).toContainText("Your username and/or password do not match");
-    //await delay(4000);
-})
-
-function delay(time) {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, time)
-    });
-}
-
-
-test("Talk to us form submission", async ({ page }) => {
-    await page.goto("https://electroneek.com/blog/");
-    await page.locator("text = Talk to us").click();
-
-    await page.fill("[name = firstname]", "testing");
-    await page.fill("[name = lastname]", "tester");
-    await page.fill("[name = email]", "testing@tester.test");
-    await page.fill("[name = phone]", "0000000000");
-    await page.locator("[name = country_list]").selectOption("United States");
-    await page.locator("[name = are_you_a_managed_services_provider_]").selectOption("No");
-    await page.locator("[name = agree]").check();
-    //await delay(4000);
-    await page.click("text = Submit");
-    await expect(page.locator("text = We’ve received your message!")).toBeVisible();
-    //await delay(4000);
 })
--- tests/positive.spec.ts
+++ tests/positive.spec.ts
@@ -1,20 +1,24 @@
 import { test, expect } from "@playwright/test";
 
+test("Talk to us form submission", async ({ page }) => {
+    await page.goto("https://electroneek.com/blog/");
+    await page.locator("text = Talk to us").click();
+    await page.fill("[name = firstname]", "testing");
+    await page.fill("[name = lastname]", "tester");
+    await page.fill("[name = email]", "testing@tester.test");
+    await page.fill("[name = phone]", "0000000000");
+    await page.locator("[name = country_list]").selectOption("United States");
+    await page.locator("[name = are_you_a_managed_services_provider_]").selectOption("No");
+    await page.locator("[name = agree]").check();
+    await page.click("text = Submit");
+    await expect(page.locator("text = We’ve received your message!")).toBeVisible();
+})
 
 test("Subscribe submission", async ({ page }) => {
     await page.goto("https://electroneek.com/blog/");
-    await page.locator('//button[text()="Subscribe"]').click();
-    await page.fill("[placeholder = Corporate email]", "testing@tester.test");
+    await page.locator('button:is(:text("Subscribe"))').click();
+    await page.fill("[name = email]", "testing@tester.test");
     await page.locator("[name = agree]").check();
-    await delay(4000);
     await page.click("text = Submit");
     await expect(page.locator("text = Thank you for the subscription!")).toBeVisible();
-    await delay(4000);
-    //await page.pause();
 })
-
-function delay(time) {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, time)
-    });
-}
